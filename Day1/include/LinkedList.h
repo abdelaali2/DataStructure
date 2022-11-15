a33e7714f167066bf1cfebdcf96b83f4c31df1ed
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2022-11-15 17:54:02 +0200

    adding files

---
+++ Day1/include/LinkedList.h
@@ -0,0 +1,148 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct Node Node;
+
+struct Node
+{
+    int Data;
+    Node *Prev, *Next;
+};
+
+/*
+typedef struct LinkedList
+{
+    Node *head, *tail;
+
+}LinkedList;
+*/
+
+Node *head = NULL, *tail = NULL;
+
+/*void Add(LinkedList *L, int data)
+{
+    L->head
+}*/
+
+void Add(int data)
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->Data = data;
+    newNode->Prev = newNode->Next = NULL;
+
+    if(head == NULL)
+    {
+        head = tail = newNode;
+    }
+    else
+    {
+        tail->Next = newNode;
+        newNode->Prev = tail;
+        tail = newNode;
+    }
+}
+
+void Display()
+{
+    Node *current = head;
+
+    while(current != NULL)
+    {
+        printf("%d   ", current->Data);
+        current = current->Next;
+    }
+}
+
+Node* GetNodeByData(int data)
+{
+    Node *current = head;
+
+    while(current != NULL)
+    {
+        if(data == current->Data)
+            return current;
+
+        current = current->Next;
+    }
+
+    return NULL;
+}
+
+void Remove(int data)
+{
+    Node *node = GetNodeByData(data);
+
+    if(node == NULL)
+        return;
+
+    if(node == head)
+    {
+        if(head == tail)
+        {
+            head = tail = NULL;
+        }
+        else
+        {
+            head = head->Next;
+            head->Prev = NULL;
+        }
+    }
+    else if(node == tail)
+    {
+        tail = tail->Prev;
+        tail->Next = NULL;
+    }
+    else
+    {
+        /*Node *A = node->Prev;
+        Node *B = node->Next;
+
+        A->Next = B;
+        B->Prev = A;*/
+
+        node->Prev->Next = node->Next;
+        node->Next->Prev = node->Prev;
+    }
+
+    free(node);
+}
+
+void InsertAfter(int data, int afterData)
+{
+    Node *current = head;
+
+    while(current != NULL)
+    {
+        if(afterData == current->Data)
+        {
+            Node *newNode = malloc(sizeof(Node));
+            newNode->Prev = current;
+            printf("%d\n",newNode->Prev->Data);
+            newNode->Data = data;
+            printf("%d\n",newNode->Data);
+            newNode->Next = current->Next;
+            printf("%d\n",newNode->Next->Data);
+            current->Next->Prev=newNode;
+            current->Next=newNode;
+            return;
+        }
+
+        current = current->Next;
+    }
+
+}
+
+int GetCount()
+{
+    Node *current = head;
+    int num;
+    while(current != NULL)
+    {
+        num++;
+        current = current->Next;
+    }
+    return num;
+}
+
+int GetDataByIndex(int index);
+#endif // LINKEDLIST_H
